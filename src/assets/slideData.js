29ce8556dcc4146eb512517836c879a4410a612d
Libina-Rai <libinarai@gmail.com>
2024-11-25 22:39:14 +0545

    bootstrap install

---
+++ src/assets/slideData.js
@@ -0,0 +1,22 @@
+// import { Home } from "../components/home/Home";
+import img1 from '../assets/images/fo.jpg'
+import img2 from '../assets/images/food.jpg'
+import img3 from '../assets/images/juice.jpg'
+
+  export const slides = [
+    {
+      "id": '1',
+      "src": img1,
+      "alt": "photo1"
+    },
+    {
+      "id": '2',
+      "src": img2,
+      "alt": "photo2"
+    },
+    {
+      "id": '3',
+      "src": img3,
+      "alt": "photo3"
+    }
+  ]
